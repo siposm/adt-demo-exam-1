c8f18ca54ec285d98a7ea5c9ca388d337806d6c7
Sipos Miklos <commit@siposm.hu>
2022-03-17 13:22:33 +0100

    added moviecreator logic

---
+++ movies/movies/MovieCreator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace movies
+{
+    public static class MovieCreator
+    {
+        public static IEnumerable<Movie> CreateMovies()
+        {
+            Func<string, IEnumerable<Movie>> func = (x) =>
+            {
+                List<Movie> movies = new List<Movie>();
+                XDocument xdoc = XDocument.Load(x);
+
+                foreach (XElement movieNode in xdoc.Descendants("Movie"))
+                {
+                    Movie m = new Movie();
+                    m.Title = movieNode.Element("Title").Value;
+                    m.Genre = movieNode.Element("Genre").Value;
+                    m.YearOfRelease = int.Parse(movieNode.Element("YearOfRelease").Value);
+                    movies.Add(m);
+                }
+
+                return movies;
+            };
+
+            return func("https://users.nik.uni-obuda.hu/siposm/db/Movies.xml");
+        }
+    }
+}
