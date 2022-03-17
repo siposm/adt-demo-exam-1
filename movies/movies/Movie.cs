3e3795bf4d8a97b49c183e42fc075175bdde0ed0
Sipos Miklos <commit@siposm.hu>
2022-03-17 12:36:06 +0100

    added entity class

---
+++ movies/movies/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movies
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(80)] 
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int YearOfRelease { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} {Title} {Genre} {YearOfRelease}";
+        }
+    }
+}
