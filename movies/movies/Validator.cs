5026ee4ed0584737e212db7460cea3addf6565fa
Sipos Miklos <commit@siposm.hu>
2022-03-17 14:25:41 +0100

    validator class created

---
+++ movies/movies/Validator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movies
+{
+    public class Validator
+    {
+        public static bool IsValid<T>(string propertyName, T obj)
+        {
+            PropertyInfo prop = typeof(T).GetProperty(propertyName);
+            
+            MinimumYearAttribute attr = (MinimumYearAttribute)prop
+                .GetCustomAttributes(typeof(MinimumYearAttribute), false)
+                .SingleOrDefault();
+            
+            int propertyValue = (int)prop.GetValue(obj);
+
+            if (attr != null && propertyValue >= attr?.MinimumYear)
+                return true;
+            else if (attr != null && propertyValue < attr?.MinimumYear)
+                return false;
+            else
+                throw new ArgumentException();
+        }
+    }
+}
