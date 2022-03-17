26a01f089153f36c26d3d674f9665a46f70f884e
Sipos Miklos <commit@siposm.hu>
2022-03-17 11:25:10 +0100

    connection string updated

--- movies/movies/Program.cs
+++ movies/movies/Program.cs
@@ -6,7 +6,7 @@ namespace movies
     {
         static void Main(string[] args)
         {
-            // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\SIPOSM\dev\adt-demo-exam-1\movies\movies\Movies.mdf;Integrated Security=True
+            // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Movies.mdf;Integrated Security=True
         }
     }
 }
