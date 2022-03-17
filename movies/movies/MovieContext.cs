933ffae3e93cd9a3296bcf47be9dc8bfdc1190da
Sipos Miklos <commit@siposm.hu>
2022-03-17 12:36:13 +0100

    added context db class

---
+++ movies/movies/MovieContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movies
+{
+    public class MovieContext : DbContext
+    {
+        public virtual DbSet<Movie> Movies { get; set;}
+
+        public MovieContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder ob)
+        {
+            if(!ob.IsConfigured)
+            {
+                ob.UseLazyLoadingProxies().UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Movies.mdf;Integrated Security=True");
+            }
+        }
+    }
+}
